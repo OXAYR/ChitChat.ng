f6236a59120e490e236417f8c08ebd15ee625759
Oxayr <uzairmaqsood451@gmail.com>
2024-10-25 15:48:12 +0500

    added the login form

--- src/app/components/master/master.component.ts
+++ src/app/components/master/master.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
+import { LoginPageComponent } from '../login-page/login-page.component';
 
 @Component({
   selector: 'app-master',
   standalone: true,
-  imports: [],
+  imports: [LoginPageComponent],
   templateUrl: './master.component.html',
-  styleUrl: './master.component.css'
+  styleUrl: './master.component.css',
 })
-export class MasterComponent {
-
-}
+export class MasterComponent {}

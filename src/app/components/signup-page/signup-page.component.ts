af22c09d9c8ad236b7b3da5d25da42a957c814c8
Oxayr <uzairmaqsood451@gmail.com>
2024-10-31 12:30:18 +0500

    added signup functionalitu

--- src/app/components/signup-page/signup-page.component.ts
+++ src/app/components/signup-page/signup-page.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-signup-page',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './signup-page.component.html',
-  styleUrl: './signup-page.component.css'
+  styleUrl: './signup-page.component.css',
 })
 export class SignupPageComponent {
+  email: string = '';
+  password: string = '';
+  userName: string = '';
 
+  login() {
+    console.log('email:', this.email);
+    console.log('Password:', this.password);
+    console.log('Password:', this.userName);
+    // Add further login logic here
+  }
 }
